1d2f835a717334467cabc55006241002bd9cc0a0
Jakub Hirsch <kuba.hirsch@gmail.com>
2021-12-18 19:49:58 +0100

    organizing project commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from random import randrange
+from random import random, randrange
 from moviedb import photos, filmwebMovies
 
 
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 
 class Changing:
-    x = randrange(125)
+    random_number = randrange(125)
     score = 0
 
 
-    def change_x(self):
-        self.x = randrange(125)
+    def change_random_number(self):
+        self.random_number = randrange(125)
         return
 
     def add_point(self):
@@ -32,30 +32,30 @@ def home():
     if request.method == "POST":
         if(request.form.get("action1") == "Zaczynamy" or request.form.get("action1") == "graj jeszcze raz"):
             changing.zero_score()
-            changing.change_x()
-            return render_template("question_page.html", imgUrl=filmwebMovies[changing.x]["url"])
+            changing.change_random_number()
+            return render_template("question_page.html", imgUrl=filmwebMovies[changing.random_number]["url"])
             
         user_input = request.form.get("input")
 
         if(request.form.get("action1") == "next"):
-            changing.change_x()
+            changing.change_random_number()
             changing.add_point()
-            return render_template("question_page.html", imgUrl=filmwebMovies[changing.x]["url"], wynik = changing.score)
+            return render_template("question_page.html", imgUrl=filmwebMovies[changing.random_number]["url"], wynik = changing.score)
 
         if(request.form.get("action1") == "Sprawdz"):
-            if(user_input.lower()+" " != filmwebMovies[changing.x]["title"].lower()):
+            if(user_input.lower()+" " != filmwebMovies[changing.random_number]["title"].lower()):
                 temp = changing.score
                 changing.zero_score()
-                return render_template("incorrect.html", wynik = temp, imgTitle = filmwebMovies[changing.x]["title"])
+                return render_template("incorrect.html", wynik = temp, imgTitle = filmwebMovies[changing.random_number]["title"])
             else:
-                return render_template("correct.html", imgUrl=filmwebMovies[changing.x]["url"], wynik = changing.score, imgTitle = filmwebMovies[changing.x]["title"], year = filmwebMovies[changing.x]["year"] )
+                return render_template("correct.html", imgUrl=filmwebMovies[changing.random_number]["url"], wynik = changing.score, imgTitle = filmwebMovies[changing.random_number]["title"], year = filmwebMovies[changing.random_number]["year"] )
             
         if(request.form.get("action1") == "koniec gry"):
             changing.zero_score()
-            changing.change_x()
+            changing.change_random_number()
             return render_template("index.html")
 
-    return render_template("index.html", imgUrl=photos[changing.x]["url"])
+    return render_template("index.html", imgUrl=photos[changing.random_number]["url"])
 
     
 
