c14b91620400b36c1473fb6e201769be454f2609
Jakub Hirsch <kuba.hirsch@gmail.com>
2021-12-19 18:42:38 +0100

    favicon change

--- app.py
+++ app.py
@@ -7,28 +7,29 @@ app = Flask(__name__)
 
 
 
-class Changing:
-    random_number = randrange(124)
-    score = 0
 
-
-    def change_random_number(self):
-        self.random_number = randrange(124)
-        return
-
-    def add_point(self):
-        self.score+=1
-        return
-
-    def zero_score(self):
-        self.score = 0
-        return
-    
-
-changing = Changing()
 
 @app.route('/', methods=["POST", "GET"])
 def home():
+    class Changing:
+        random_number = randrange(124)
+        score = 0
+
+
+        def change_random_number(self):
+            self.random_number = randrange(124)
+            return
+
+        def add_point(self):
+            self.score+=1
+            return
+
+        def zero_score(self):
+            self.score = 0
+            return
+            
+    changing = Changing()
+
     if request.method == "POST":
         if(request.form.get("action1") == "Zaczynamy" or request.form.get("action1") == "graj jeszcze raz"):
             changing.zero_score()
